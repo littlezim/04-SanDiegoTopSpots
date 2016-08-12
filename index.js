b74b21ea9c7a20cb018904f7305a1fd5cc465461
littlezim <littlezim18@gmail.com>
2016-08-12 12:43:08 -0700

    sandiegotopspots

---
+++ index.js
@@ -0,0 +1,61 @@
+/* $(document).ready(function(){
+	$.getJson('topspots.json' , function(topspots){
+	$(+ topspot.location[0] + '">Click here to open in google"		
+
+
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+$(document).ready(function(){
+	$.getJson("topspots.json", function(result){
+//learn arrays
+
+		 topspots [0]
+		console.log(topspots);
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
